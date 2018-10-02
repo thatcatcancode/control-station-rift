83f4a591d48860780818c99b0ebc2728d4cf96f1
Dustin Rodda <drodda@atlatlsoftware.com>
2018-10-01 22:02:12 -0400

    Updated the indicators to take cues from the camera

--- Assets/Scripts/HeadingRotator.cs
+++ Assets/Scripts/HeadingRotator.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadingRotator : MonoBehaviour {
-
+public class HeadingRotator : MonoBehaviour
+{
+    public Transform camera;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+	void LateUpdate () {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, camera.eulerAngles.y);
 	}
 }
--- Assets/Scripts/PitchMovement.cs
+++ Assets/Scripts/PitchMovement.cs
@@ -9,42 +9,55 @@ public class PitchMovement : MonoBehaviour
     bool initialPosFilled = false;
     int steps = 1;
     Vector3 initialPos;
+    public Transform camera;
+    float previousX;
+    float deltaX;
     // Use this for initialization
     void Start()
     {
-
+        previousX = camera.eulerAngles.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        Quaternion theQuat = new Quaternion(0f, 0f, 0f, 0f);
-        Vector3 position;
+        float cameraXRotation = camera.eulerAngles.x;
+        Debug.Log(camera.eulerAngles.x);
+        if(cameraXRotation > 180)
+        {
+            cameraXRotation -= 360;
+        }
+        deltaX = previousX - cameraXRotation;
+        transform.position = new Vector3(transform.position.x, transform.position.y + deltaX, transform.position.z);
+        previousX = cameraXRotation;
 
-        if (initialPosFilled == false)
-        {
-            initialPos = transform.position;
-            initialPosFilled = true;
-        }
+        //Quaternion theQuat = new Quaternion(0f, 0f, 0f, 0f);
+        //Vector3 position;
 
-        position = new Vector3(initialPos.x, initialPos.y + .6f * steps, 0);
-        transform.SetPositionAndRotation(position, theQuat);
-        if (steps >= numStepsUp )
-        {
-           up = !up;
-        }
-        if (steps == 0)
-        {
-            up = !up;
-        }
+        //if (initialPosFilled == false)
+        //{
+        //    initialPos = transform.position;
+        //    initialPosFilled = true;
+        //}
 
-        if(up == true)
-        {
-            ++steps;
-        }
-        else
-        {
-            --steps;
-        }
+        //position = new Vector3(initialPos.x, initialPos.y + .6f * steps, 0);
+        //transform.SetPositionAndRotation(position, theQuat);
+        //if (steps >= numStepsUp )
+        //{
+        //   up = !up;
+        //}
+        //if (steps == 0)
+        //{
+        //    up = !up;
+        //}
+
+        //if(up == true)
+        //{
+        //    ++steps;
+        //}
+        //else
+        //{
+        //    --steps;
+        //}
     }
 }
