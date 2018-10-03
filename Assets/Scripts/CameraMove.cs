b668e19bf129001bb0c2e808a347bc8d83f2d3a1
Dustin Rodda <drodda@atlatlsoftware.com>
2018-10-02 22:42:10 -0400

    Added joystick controls

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -2,38 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour {
+public class CameraMove : MonoBehaviour
+{
+    string xAxis = "Pitch";
+    string yAxis = "Yaw";
+    string zAxis = "Roll";
 
-    Vector3 axis;
+    bool isMouseBeingUsed = false;
 
-    // Use this for initialization
-    void Start () {
-        //axis = GetComponent<Renderer>().bounds.center;
+	void Update ()
+    {
+        RotateCamera();
+        CheckForAppQuit();
     }
-	
-	// Update is called once per frame
-	void LateUpdate () {
 
-        //Capturing mouse movements for smoother experience
-        //RotateAround is deprecated
-        transform.Rotate(transform.right, Input.GetAxis("Mouse Y"));
-        transform.Rotate(transform.up, Input.GetAxis("Mouse X"));
+    void RotateCamera()
+    {
+        CheckForMouse();
+        Debug.Log(isMouseBeingUsed);
+        xAxis = isMouseBeingUsed ? "Mouse Y" : "Pitch";
+        yAxis = isMouseBeingUsed ? "Mouse X" : "Yaw";
 
-        //if (Input.GetKeyDown(KeyCode.W))
-        //{
-        //    transform.Rotate(transform.right, 10);
-        //}
-        //else if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    transform.RotateAround(axis, new Vector3(0, 1, 0), -10);
-        //}
-        //else if (Input.GetKeyDown(KeyCode.S))
-        //{
-        //    transform.RotateAround(axis, new Vector3(1, 0, 0), -10);
-        //}
-        //else if (Input.GetKeyDown(KeyCode.D))
-        //{
-        //    transform.RotateAround(axis, new Vector3(0, 1, 0), 10);
-        //}
+        transform.Rotate(transform.right, Input.GetAxis(xAxis));
+        transform.Rotate(transform.up, Input.GetAxis(yAxis));
+        if(!isMouseBeingUsed)
+        {
+            transform.Rotate(transform.forward, Input.GetAxis(zAxis));
+        }
+    }
+
+    void CheckForMouse()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            isMouseBeingUsed = !isMouseBeingUsed;
+        }
+    }
+
+
+    void CheckForAppQuit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Quit");
+        }
     }
 }
--- Assets/Scripts/RollRotator.cs
+++ Assets/Scripts/RollRotator.cs
@@ -2,43 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RollRotator : MonoBehaviour {
-
-    bool clockwise = true;
-    bool first = true;
-    int steps = 0;
-    int maxSteps = 100;
+public class RollRotator : MonoBehaviour
+{
     public Transform camera;
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
 	void LateUpdate ()
     {
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, camera.eulerAngles.z);
-        //++steps;
-
-        //if (clockwise == true)
-        //{
-        //    transform.Rotate(new Vector3(0, 0, 20) * Time.deltaTime);
-        //}
-        //else
-        //{
-        //    transform.Rotate(new Vector3(0, 0, -20) * Time.deltaTime);
-        //}
-         
-        //if(steps > maxSteps/2 && first == true)
-        //{
-        //    clockwise = !clockwise;
-        //    steps = 0;
-        //    first = false;
-        //}
-        //if (steps > maxSteps && first == false)
-        //{
-        //    clockwise = !clockwise;
-        //    steps = 0;
-        //}
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, camera.localEulerAngles.z);
     }
 }
